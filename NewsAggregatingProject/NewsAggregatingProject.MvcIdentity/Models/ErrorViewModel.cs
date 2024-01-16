fba5965029bd4f89f47e88dea47a88bf8d2a0a78
marla124 <95192268+marla124@users.noreply.github.com>
2024-01-16 19:41:22 +0300

    MvcIdentity

---
+++ NewsAggregatingProject/NewsAggregatingProject.MvcIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewsAggregatingProject.MvcIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- NewsAggregatingProject/NewsAggregatingProject.Services/obj/Debug/net8.0/NewsAggregatingProject.Services.AssemblyInfo.cs
+++ NewsAggregatingProject/NewsAggregatingProject.Services/obj/Debug/net8.0/NewsAggregatingProject.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewsAggregatingProject.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c104eee44b36a7e639d784376f43ef0f487e25b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewsAggregatingProject.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewsAggregatingProject.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
