2925b26ca35aae4e95252fe8d6cd5bf43ee8fe97
marla124 <95192268+marla124@users.noreply.github.com>
2024-01-17 22:24:30 +0300

    fix logout

--- NewsAggregatingProject/NewsAggregatingProject/obj/Debug/net8.0/NewsAggregatingProject.AssemblyInfo.cs
+++ NewsAggregatingProject/NewsAggregatingProject/obj/Debug/net8.0/NewsAggregatingProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewsAggregatingProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba5965029bd4f89f47e88dea47a88bf8d2a0a78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adc4301d7af96662e19bb7675e52b6337a08f857")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewsAggregatingProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewsAggregatingProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
