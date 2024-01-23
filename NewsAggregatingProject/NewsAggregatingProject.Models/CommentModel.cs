166ab5a7c0d9f42563e8bf7bc90662c265214102
marla124 <95192268+marla124@users.noreply.github.com>
2024-01-23 22:23:30 +0300

    add new model

---
+++ NewsAggregatingProject/NewsAggregatingProject.Models/CommentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsAggregatingProject.Models
+{
+    internal class CommentModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Text { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public Guid NewsId { get; set; }
+        public Guid? ParentCommentId { get; set; }
+    }
+}
---
+++ NewsAggregatingProject/NewsAggregatingProject.Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsAggregatingProject.Models
+{
+    internal class UserModel
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Guid UserStatusId { get; set; }
+    }
+}
